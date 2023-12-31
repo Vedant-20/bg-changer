0e9834187fedb82144422091acc5b149e9580ab9
Vedant <vedantwarjurkar@gmail.com>
2023-12-31 10:21:28 +0530

    background color switcher

---
+++ script.js
@@ -0,0 +1,34 @@
+const buttons=document.querySelectorAll('.button');
+
+const body=document.querySelector('body');
+
+buttons.forEach((button)=>{
+    button.addEventListener('click',(e)=>{
+        switch (e.target.id) {
+            case 'grey':
+                body.style.backgroundColor=e.target.id;
+                
+                
+            
+            case 'yellow':
+                body.style.backgroundColor=e.target.id;
+                
+                
+            case 'blue':
+                body.style.backgroundColor=e.target.id;
+                
+                
+            case 'white':
+                body.style.backgroundColor=e.target.id;
+                    
+            case 'purple':
+                body.style.backgroundColor=e.target.id;
+
+            case 'orange':
+                body.style.backgroundColor=e.target.id;
+                    
+            default:
+                break;
+        }
+    })
+})
